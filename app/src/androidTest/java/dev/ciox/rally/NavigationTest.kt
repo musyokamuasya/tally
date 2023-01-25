9f1b449deb337cc48b1a0904cecc25dd9455d9d3
musyokamuasya <josephmuasya8@gmail.com>
2023-01-25 17:18:48 +0300

    Test for navigation and clicks

--- app/src/androidTest/java/dev/ciox/rally/NavigationTest.kt
+++ app/src/androidTest/java/dev/ciox/rally/NavigationTest.kt
@@ -4,9 +4,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.testing.TestNavHostController
 import dev.ciox.rally.ui.components.RallyNavHost
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -30,5 +34,21 @@ class NavigationTest {
     @Test
     fun rallyNavHost_verifyStartDestination() {
         composeTestRule.onNodeWithContentDescription("Overview Screen").assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_clickAllAccounts_navigatesToAllAccounts() {
+        composeTestRule.onNodeWithContentDescription("All Accounts").performClick()
+        composeTestRule.onNodeWithContentDescription("Accounts Screen").assertIsDisplayed()
+    }
+
+    @Test
+    fun rallyNavHost_clickAllBills_navigatesToAllBills() {
+        composeTestRule.onNodeWithContentDescription("All Bills")
+            .performScrollTo()
+            .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, "bills")
+
     }
 }
